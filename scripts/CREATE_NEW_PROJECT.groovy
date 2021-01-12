d923748d0e88714689cc952aebb05d0956f40d6e
e600685 <sanath.kumar.dharmasthala@evry.com>
2021-01-12 12:20:13 +0530

    Enhanced scripts for creating new project
    Also added new test suite for other groovy scripts parking which can be kept as a backup or for quick reference

--- scripts/CREATE_NEW_PROJECT.groovy
+++ scripts/CREATE_NEW_PROJECT.groovy
@@ -1,24 +1,31 @@
 //CREATE_NEW_PROJECT
 
 class CREATE_NEW_PROJECT {
-    public static void create ( String projectName , testRunner)
+    public static void create ( String projectName , context)
     {
-        def project = testRunner.testCase.testSuite.project;
+        def randomValue = java.util.UUID.randomUUID().toString().substring(0,13);
+        def project = context.testCase.testSuite.project;
         def workspace = project.workspace;
-        def porjectExists = null;
-        porjectExists = workspace.getProjectByName (projectName);
-        if (porjectExists != null)
+        def projPath = new com.eviware.soapui.support.GroovyUtils(context).projectPath //gets the path of the project root
+        def projectExists = null;
+        projectExists = workspace.getProjectByName (projectName);
+        println ("project Existance --> "+projectExists.getPath()+"___"+projectExists.getName())
+        
+        if (projectExists == null)
         {
-            def newProject = workspace.createProject(projectName,new File("C:/Users/Ei06593/OneDrive - EVRY/DOCUMENTS_SANATH/testSampleProject.xml")) ;
+            def newProject = workspace.createProject(projectName,new File(projPath+"/"+projectName+".xml")) ;
             projectName = newProject.getName();
             println ("NEW PROJECT IS CREATED WITH NAME: "+projectName);
+            projectExists = workspace.getProjectByName (projectName);
         }
-        project = workspace.getProjectByName(projectName);
-        
-        def testSuite = project.addNewTestSuite( "Sample_TestSuite" );
-        def testCase = testSuite.addNewTestCase( "Sample_TestCase" );
-        def testStep = testCase.addTestStep( "groovy", "Sample_GroovyStep" );
-        
-        println ( "CREATED NEW TEST SUITE :-"+ testSuite.getName() +" AND NEW TEST CASE :"+ testCase.getName() + " AND ONE NEW TEST STEP :"+ testStep.getName() );
+        if (projectExists != null)
+        {
+            def testSuite = project.addNewTestSuite( "Sample_TestSuite_"+randomValue );
+            println ( "CREATED NEW TEST SUITE --> "+ testSuite.getName());
+            def testCase = testSuite.addNewTestCase( "Sample_TestCase_"+randomValue );
+            println ( "CREATED NEW TEST CASE ---> "+ testCase.getName());
+            def testStep = testCase.addTestStep( "groovy", "Sample_GroovyStep_"+randomValue );
+            println ( "CREATED NEW TEST STEP ---> "+ testStep.getName());
+        }
     }
 }
