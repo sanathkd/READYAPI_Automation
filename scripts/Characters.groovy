2a102b7cd8d237afedd9c8521ff0ca7f3b0d3ddb
Sanath Kumar Dharmasthala <Sanath.Kumar.Dharmasthala@tietoevry.com>
2024-02-22 14:47:58 +0530

    NO_TASK : More Changes

--- scripts/Characters.groovy
+++ scripts/Characters.groovy
@@ -1,11 +1,12 @@
 //	Shuffle Characters of a string
 class Characters{
-	public static void shuffle(String word) {
-		System.out.println("Before: " + word + " \nAfter : " + scramble(word.toCharArray()) );
+	public void shuffle(String word) {
+		System.out.println("TEST");
+		System.out.println("Before: " + word + " \nAfter : " + scramble(word) );
 	}
-	public static String scramble( String inputString ){
+	public String scramble( String inputString ){
 		Random random = new Random();
-		String stringArray = inputString;//.toCharArray();
+		String stringArray = inputString.toCharArray();
 		for( int i=0 ; i<stringArray.length() ; i++ ){
 			int j = random.nextInt(stringArray.length);
 			char holder;
